b0f9e1cc8068d98ffd67d6e43f1485dd51cb1aea
farimakonare <konarefarima0@gmail.com>
2025-10-09 12:13:17 +0000

    added other apis

---
+++ src/app/api/cart-items/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const items = await prisma.cartItem.findMany({
+    include: { product: true, cart: true },
+  });
+  return NextResponse.json(items);
+}
+
+export async function POST(req: Request) {
+  const data = await req.json();
+  const item = await prisma.cartItem.create({ data });
+  return NextResponse.json(item);
+}
---
+++ src/app/api/reviewss/review.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const reviews = await prisma.review.findMany({
+    include: { user: true, product: true },
+  });
+  return NextResponse.json(reviews);
+}
+
+export async function POST(req: Request) {
+  const data = await req.json();
+  const review = await prisma.review.create({ data });
+  return NextResponse.json(review);
+}
